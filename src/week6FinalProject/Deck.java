1ff9b00301419d80e59f737ac8e7379a8a05aeea
scollins97 <srcollins575@gmail.com>
2022-01-20 21:27:56 -0500

    comment cleanup

--- src/week6FinalProject/Deck.java
+++ src/week6FinalProject/Deck.java
@@ -12,18 +12,18 @@ public class Deck {
 		String[] cardSuits = {"Hearts" , "Spades" , "Diamonds" , "Clubs"};
 		for(int i = 0; i < 4; i++) {                                  
 			for(int j = 2; j <= 14; j++) {                                											  	
-					Card temp = new Card(j, cardSuits[i]);//was i supposed to use the setters for this part?
+					Card temp = new Card(j, cardSuits[i]);
 					newDeck.add(temp);
-			}//i think this entire thing should be in the field rather than than a private method even though it works
+			}
 		}	
-return newDeck;
-}
-public void shuffle() {
+		return newDeck;
+	}	
+	public void shuffle() {
 	Collections.shuffle(cards);
-}
-public Card draw() {//takes the first value of the deck, stores it in temp, then deletes from cards. returns that card
+	}
+	public Card draw() {//takes the first value of the deck, stores it in temp, then deletes from cards. returns that card
 	return cards.remove(0);
-}
+	}
 
 
 }
